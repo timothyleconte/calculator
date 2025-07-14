2a07d732664157876c7bb94c929904aea652a754
timothyleconte <marktbooher@outlook.com>
2025-07-14 00:39:26 -0400

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,27 @@
+function add(a, b){
+let sum = a + b;
+return sum;
+};
+
+console.log(add(32, 10));
+
+function subtract(a, b){
+    let sum = a - b;
+    return sum;
+};
+
+console.log(subtract(100, 73));
+
+function multiply(a, b){
+    let sum = a * b;
+    return sum;
+};
+
+console.log(multiply(2, 30));
+
+function divide(a, b){
+    let sum = a / b;
+    return sum;
+}
+
+console.log(divide(9, 3));
