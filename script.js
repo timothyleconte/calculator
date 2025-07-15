104c666fd4f281e64c2371c59becbd14dc168ae5
timothyleconte <marktbooher@outlook.com>
2025-07-14 23:03:24 -0400

    Add button selectors and operate function

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ divide = (a, b) => a / b;
 let operator;
 let nOne;
 let nTwo;
+
+let addButton = document.querySelector(".addition");
+let subtractButton = document.querySelector(".subtraction");
+let multiplyButton = document.querySelector(".multiplication");
+let divideButton = document.querySelector(".division");
+
+
+function operate(operator, a, b){
+
+    if (operator === addButton){
+        return add(a, b);
+    }else if (operator === subtractButton){
+        return subtract(a, b);
+    }else if(operator === multiplyButton){
+        return multiply(a, b);
+    }else if(operator === divideButton){
+        return divide(a, b);
+    }else{
+        return "null";
+    }
+
+}
+
+console.log(operate(addButton, 3, 7))
+
+console.log(operate(divideButton, 10, 5));
+
+console.log(operate(multiplyButton, 7, 3));
+
+console.log(operate(subtractButton, 3, 1));
